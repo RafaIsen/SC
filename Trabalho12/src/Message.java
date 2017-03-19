750fe32ac41442923d22afaa84e5f9d429ce714a
HP <HP@192.168.1.82>
2017-03-19 22:10:54 +0000

    trabalho 12

---
+++ Trabalho12/src/Message.java
@@ -0,0 +1,29 @@
+import java.io.Serializable;
+import java.util.Date;
+
+public class Message implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -293738311238270888L;
+	public String method;
+	public String[] fileName;
+	public String repName;
+	public Date[] fileDate;
+	public boolean[] toBeUpdated; 
+	public String[] user;
+	public boolean[] delete;
+	public String result;
+	
+	public Message(String method, String[] fileName, String repName, Date[] fileDate, boolean[] toBeUpdated, String[] user, boolean[] delete, String result) {
+		this.method = method;
+		this.fileName = fileName;
+		this.repName = repName;
+		this.fileDate = fileDate;
+		this.toBeUpdated = toBeUpdated;
+		this.user = user;
+		this.delete = delete;
+		this.result = result;
+	}
+
+}
